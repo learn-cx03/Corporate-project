43ba69ecd990f8f4c26c8505732c53019827c11d
cx <2716506732@qq.com>
2022-06-01 10:43:50 +0800

    change input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState, useRef} from 'react'
 import './App.css'
 
 //引入轮播图组件
@@ -10,40 +10,27 @@ import search from './img/search.png'
 
 export default function App() {
   //用useState生成的变量实现变量变换在页面的实时更新
-  const [useShow, setUseShow] = useState(true)
-  const [useInput, setUseInput] = useState(false);
   const [height, setHeight] = useState(0)
   const [useWidth, setUseWidth] = useState(0)
-  const [useText, setUseText] = useState('123')
-
-  //通过点击实现useShow变量的布尔值取反，从而操作组件的显示与隐藏
+  const inputE1 = useRef(null)
+  //通过点击实现height变量的改变，从而操作组件的移动
   const changeShow = () => {
-    console.log('关闭')
-    setUseShow(!useShow)
-    setUseInput(false);
     if(height) {
       setHeight(0)
     } else {
       setHeight(277)
     }
   }
+  //通过点击搜索图片，展开input 的函数
   const changeInput = () => {
     if (useWidth) {
+      console.log(inputE1.current.value)
       setUseWidth(0)
+      inputE1.current.value = null
     } else {
       setUseWidth(340)
     }
   }
-  const changeFocus = (e) => {
-    setUseText(e.target.value)
-    console.log(useText)
-  }
-  const getInput = (e) => {
-    if (e.key == 'Enter') {
-      console.log('dddd')
-      setUseText('')
-    }
-  } 
   return (
     <div className='test'>
       {/* 通过visibility来实现组件的显示与隐藏 */}
@@ -55,8 +42,8 @@ export default function App() {
           <div className={height ? 'footer-left' : 'headertest'} ></div>
           <div className='headerlist' style={{display: height ?  'none' : 'block'}}>
           <div className='input_box' style={{width: `${useWidth}px`, height: "50px", transition: '0.8s'}}>
-            <input type="text" className='input'/>
-            <img src={search} className='input_img' onClick={changeInput}/>
+            <input type="text" className='input' ref={inputE1}/>
+            <img src={search} className='input_img' onClick={changeInput} />
           </div>
           <div className='headerList2'  style={{display: height ?  'none' : 'block'}}>
             <img src={search} style={{width: "25px", height: "25px"}} onClick={changeInput} className="search"/>
--- src/TestMy.jsx
+++ src/TestMy.jsx
@@ -4,13 +4,21 @@ import img2 from './img/dog.jpg'
 import img3 from './img/cat.jpg'
 import img4 from './img/cat2.jpg'
 import img5 from './img/cat3.jpg'
-import './test.css'
-import { changeSlide } from 'react-slick/lib/utils/innerSliderUtils'
+import './testMy.css'
 
 
 export default function Test() {
   //定义存放图片和图片里文字的两个数组
-  const showImg = [img1, img2, img3, img4, img5, img3, img2, img1, img4]
+  const forms = [
+    'translate3d(-450px, 0, -100px) rotateY(45deg)',
+    'translate3d(-230px, 0, 0) rotateY(45deg) ',
+    'translate3d(0, 0, 100px)',
+    'translate3d(230px, 0, 0) rotateY(-45deg) ',
+    'translate3d(450px, 0, -100px) rotateY(-45deg)',
+  ]
+  const [transfrom, setTransform] = useState(forms)
+  // console.log(transfrom) 
+  const showImg = [img1, img2, img3, img4, img5, img3]
   const newArry = []
   //通过for循环遍历将循环得出的dom元素做个一个数组
   for (let i = 0; i < showImg.length; i++) {
@@ -21,39 +29,38 @@ export default function Test() {
       </div>
     )
   }
+  // console.log(newArry)
   //使用useState对数组进行操作
   const [showList, setShowList] = useState(newArry)
-  const [isShow, setIsShow] = useState('true')
-  //给每个图片盒子添加上动画，在切换盒子的时候使用
-  const animationFun = () =>  {
-    if (isShow) {
-      document.getElementsByClassName('box1')[0].className = 'box1 box1_active'
-      document.getElementsByClassName('box2') [0].className = 'box2 box2_active'
-      document.getElementsByClassName('box3') [0].className = 'box3 box3_active'
-      // document.getElementsByClassName('box4') [0].className = 'box4 box4_active'
-      // document.getElementsByClassName('box5')[0].className = 'box5 box5_active'
-    } else {
-      document.getElementsByClassName('box1')[0].className = 'box1 box1_actives'
-      document.getElementsByClassName('box2')[0].className = 'box2 box2_actives'
-      document.getElementsByClassName('box3') [0].className = 'box3 box3_actives'
-      // document.getElementsByClassName('box4') [0].className = 'box4 box4_actives'
-      // document.getElementsByClassName('box5')[0].className = 'box5 box5_actives'
-    }
-
-    setIsShow(!isShow)
-    ChangeNext()
+  const rightRotate = () => {
+    let text = transfrom[0]
+    let temp = transfrom.slice(1, transfrom.length)
+    temp.push(text)
+    setTransform(temp)
+    // console.log(temp)
   }
+  const leftRotate = () => {
+    let text = transfrom[transfrom.length - 1]
+    let temp = transfrom.slice(0, transfrom.length - 1)
+    temp.unshift(text)
+    setTransform(temp)
+    // console.log(temp)
+  }
+  // const [isShow, setIsShow] = useState('true')
+  //给每个图片盒子添加上动画，在切换盒子的时候使用
+
 //通过函数来实现对数组的里面元素的重排，从而实现对dom元素的位置更改，实现轮播的效果
 
 //ChangeLast（左箭头函数） 是通过函数将数组先前一位
-const ChangeLast = () => {
-  let text = showList[0]
-  let temp = showList.slice()
-  temp.splice(0, 1)
-  temp.push(text)
-  setShowList(temp)
-  animationFun()
-}
+  const ChangeLast = () => {
+    // let text = showList[0]
+    // let temp = showList.slice()
+    // temp.splice(0, 1)
+    // temp.push(text)
+    // setShowList(temp)
+    // console.log(temp)
+
+  }
 
 //ChangeNext（右箭头函数） 是通过函数将数组向前一位
   const ChangeNext = () => {
@@ -62,7 +69,7 @@ const ChangeLast = () => {
     temp.splice(temp.length - 1, 1)
     temp.unshift(text)
     setShowList(temp)
-    // animationFun()
+    rightRotate()
   }
 
 //ChangeList（点击图片排序到中心） 是通过函数还有传递过的index位置来对数组进行重新排序，从而实现点击的dom元素都在轮播图的中间的效果
@@ -87,7 +94,7 @@ const ChangeLast = () => {
   return (
     <div className='test-style'>
     {/* 左边的箭头 */}
-      <div className='page' onClick={ChangeLast}>
+      <div className='page' onClick={leftRotate}>
 
         {/* <div  className="page"/> */}
       </div>
@@ -97,35 +104,35 @@ const ChangeLast = () => {
           if (index < 5) { 
             if (index == 0) {
               return (
-                <div className="box1" key={index} onClick={() => ChangeList(index)} >
+                <div className="box1" key={index} onClick={() => ChangeList(index)} style={{transform: transfrom[index]}}>
                  {item}
                 </div>
               )
             }
             if (index == 1) {
               return (
-                <div className="box2" key={index} onClick={() => ChangeList(index)}>
+                <div className="box2" key={index} onClick={() => ChangeList(index)} style={{transform: transfrom[index]}}>
                   {item}
                 </div>
               )
             }
             if (index == 2) {
               return (
-                <div className="box3" key={index} onClick={() => ChangeList(index)}>
+                <div className="box3" key={index} onClick={() => ChangeList(index)} style={{transform: transfrom[index]}}>
                   {item}
                 </div>
               )
             }
             if (index == 3) {
               return (
-                <div className="box4" key={index} onClick={() => ChangeList(index)}>
+                <div className="box4" key={index} onClick={() => ChangeList(index)} style={{transform: transfrom[index]}}>
                   {item}
                 </div>
               )
             }
             if (index == 4) {
               return (
-                <div className="box5" key={index} onClick={() => ChangeList(index)}>
+                <div className="box5" key={index} onClick={() => ChangeList(index)} style={{transform: transfrom[index]}}>
                  {item}
                 </div>
               )
@@ -134,7 +141,7 @@ const ChangeLast = () => {
         })}
       </div>
     {/* 右边的箭头 */}
-      <div onClick={animationFun} className="page_h"/>
+      <div onClick={rightRotate} className="page_h"/>
     </div>
   )
 }
